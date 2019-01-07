16cd6e1112879e6c33558858c0d83c241f8512ce
nuria peralta <nuria.peralta@basetis.com>
2019-01-07 12:16:08 +0100

    Spiner added in calls inside BeerList and RecipeList

--- app/src/main/java/com/example/nuriaperalta/kotlinstart3/UI/RecipeScreen/RecipeListFragment.kt
+++ app/src/main/java/com/example/nuriaperalta/kotlinstart3/UI/RecipeScreen/RecipeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +22,7 @@ import okhttp3.Request
 import java.io.IOException
 
 class RecipeListFragment : Fragment() {
+    var progressBar : ProgressBar? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.recipe_list_layout, container, false)
@@ -30,14 +32,18 @@ class RecipeListFragment : Fragment() {
         val recyclerView = view?.findViewById<RecyclerView>(R.id.recipe_list_recycler_view)
         recyclerView?.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
 
+        progressBar = view?.findViewById(R.id.beer_list_bar)
+
         return view
     }
 
     fun recipeListCoroutine() {
         val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
         val request = Request.Builder().url(url).build()
-
         val client = OkHttpClient()
+
+        progressBar?.visibility = View.VISIBLE
+
         client.newCall(request).enqueue(object : Callback {
 
             override fun onResponse(call: Call, response: okhttp3.Response) {
@@ -49,11 +55,15 @@ class RecipeListFragment : Fragment() {
                 activity?.runOnUiThread {
                     recipe_list_recycler_view.adapter = RecipeListAdapter(recipeList)
                     recipe_list_recycler_view.adapter = RecipeListAdapter(recipeList)
+                    progressBar?.visibility = View.GONE
                 }
             }
 
             override fun onFailure(call: Call, e: IOException) {
                 Log.d("---Exception", "BeerListFragment beerListCoroutine exception -->" + e.toString())
+                activity?.runOnUiThread {
+                    progressBar?.visibility = View.GONE
+                }
             }
         })
 
